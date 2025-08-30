b4703419d2513acfe871f07230a08390957258ac
MateusArthur1027 <mateusarthur6788@gmail.com>
2025-08-30 17:13:40 -0300

    Update Método_dos_mínimos_quadrados.py

--- Método_dos_mínimos_quadrados.py
+++ Método_dos_mínimos_quadrados.py
@@ -18,25 +18,58 @@ def mínimos_quadrados(xi, yi, n):
       mx = sxi/n
       B1 = (sxiyi - ((sxi*syi)/n))/(sxi2 - ((sxi**2)/n))
       B0 = my - (B1*mx)
-      return B1, B0
+      return B1, B0, mx, my
     else:
         return print("Os vetores são de tamanhos diferentes")
 
 resp = mínimos_quadrados(xi, yi, n)
 print(f"\nB1 vale {resp[0]} e B0 vale {resp[1]}\n")
 print(f"Resultando na equação y = {resp[1]} + ({resp[0]})*x\n")
-
-def resíduos(resp, xi, yi):
-  B1xi = []
-  for i in xi:
+def resíduos(y, x, resp):
+  for i in x:
+    B1xi = []
     B1xi.append(resp[0]*i)
-  er = yi -(resp[1] + B1xi)
+  er = []
+  er.append(y - (resp[1] + B1xi))
   return er
 
-eixoy = np.array(resíduos(resp, xi, yi))
-eixox = np.arange(len(eixoy))
-plt.scatter(eixox, eixoy)
-plt.xlabel("Posição(i)")
-plt.ylabel("Valor do resíduo")
-plt.title("Valor do resíduo por posição (i) de xi e yi")
+err = resíduos(yi, xi, resp)
+def funres(x,resp):
+  if np.isscalar(x) == True:
+    ycc = resp[1] + (resp[0]*x)
+    return ycc 
+  else:
+   ycc = []
+   for i in x:
+     j = resp[1] + (resp[0]*i)
+     ycc.append(j)
+   return ycc
+yc = funres(xi, resp)
+ycm = funres(resp[2], resp)
+#Verificações:
+if np.sum(err) == 0:
+  print("A soma dos resíduos é zero\n")
+else:
+  print("A soma dos resíduos é diferente de zero\n")
+if np.sum(yi) == np.sum(yc):
+  print("A soma dos valores ajustados é a mesma da soma dos valors observados\n")
+else:
+  print("A soma dos valores ajustados é diferente da soma dos valores observados\n")
+if ycm == resp[3]:
+  print("A reta de regressão passa pelo centróide (xmédio, ymédio)\n")
+else:
+  print("A reta de regressão não passa pelo centróide(xmédio, ymédio)\n")
+for i in yc:
+  for j in err:
+    sycerr = np.sum(i*j)
+if sycerr == 0:
+  print("A soma dos resíduos ponderados pelo valor correspondente ajustado é zero\n")
+else:
+  print("A soma dos resíduos ponderados pelo valor correspondente ajustado não é zero\n")  
+
+plt.scatter(resp[2], resp[3], c='red')
+plt.scatter(xi, yi)
+plt.plot(xi, yc, c='orange')
+plt.xlabel("Tempo(semanas)")
+plt.ylabel("Propelente")
 plt.show()
